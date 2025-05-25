a9c3415e4c8891cdfec441e49b6e58d9133432e4
jiansiyu <jiansiyu@gmail.com>
2025-05-25 13:30:08 -0700

    update file

--- UI/backend/app/main.py
+++ UI/backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
-
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 app.add_middleware(
@@ -11,5 +11,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+app.mount("/static", StaticFiles(directory="./uploads"), name="static")
 app.include_router(router)
